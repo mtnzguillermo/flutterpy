a3f43173b8f4e1608b07b4352190eab2d2db4689
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-03-16 17:56:42 +0100

    Notation section structure

---
+++ derivatives/notation_utilities.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+
+class Notation():
+
+    def __init__(self):
+        pass
+
+
+class RealNotation(Notation):
+
+    def __init__(self):
+        
+        super().__init__()
+
+
+class ComplexNotation(Notation):
+
+    def __init__(self):
+
+        self.name = 
+
+        relations = {
+            'c_hh':{
+                'H4': 2/np.pi,
+                'H1': 2/np.pi*j
+            }
+        }
+
+        super().__init__()
+
