bade53b4c575392d04923e7651a4a91da2cce225
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-03-17 19:36:43 +0100

    Notation 'real' added

--- derivatives/_notation_list.py
+++ derivatives/_notation_list.py
@@ -1,72 +1,25 @@
 
 
-notation_settings = [
+notation_list = [
     {
         'name':'real',
         'relations':{
             'convert':{
-                'H1':'Ured/2/pi*l3',
-                'H2':'Ured/2/pi*l5',
-                'H3':'(Ured/2/pi)**2*l6',
-                'H4':'(Ured/2/pi)**2*l4',
-                'H5':'Ured/2/pi*l1',
-                'H6':'(Ured/2/pi)**2*l2',
-                'A1':'Ured/2/pi*m3',
-                'A2':'Ured/2/pi*m5',
-                'A3':'(Ured/2/pi)**2*m6',
-                'A4':'(Ured/2/pi)**2*m4',
-                'A5':'Ured/2/pi*m1',
-                'A6':'(Ured/2/pi)**2*m2',
-                'P1':'Ured/2/pi*d1',
-                'P2':'Ured/2/pi*d5',
-                'P3':'(Ured/2/pi)**2*d6',
-                'P4':'(Ured/2/pi)**2*d2',
-                'P5':'Ured/2/pi*d3',
-                'P6':'(Ured/2/pi)**2*d4'
+                'H1':'(Ured/2/pi)*l3',    'H2':'(Ured/2/pi)*l5',  'H3':'(Ured/2/pi)**2*l6',
+                'H4':'(Ured/2/pi)**2*l4', 'H5':'(Ured/2/pi)*l1',  'H6':'(Ured/2/pi)**2*l2',
+                'A1':'(Ured/2/pi)*m3',    'A2':'(Ured/2/pi)*m5',  'A3':'(Ured/2/pi)**2*m6',
+                'A4':'(Ured/2/pi)**2*m4', 'A5':'(Ured/2/pi)*m1',  'A6':'(Ured/2/pi)**2*m2',
+                'P1':'(Ured/2/pi)*d1',    'P2':'(Ured/2/pi)*d5',  'P3':'(Ured/2/pi)**2*d6',
+                'P4':'(Ured/2/pi)**2*d2', 'P5':'(Ured/2/pi)*d3',  'P6':'(Ured/2/pi)**2*d4'
             },
             'deconvert':{
-                'H1':'H1',
-                'H2':'H2',
-                'H3':'H3',
-                'H4':'H4',
-                'H5':'H5',
-                'H6':'H6',
-                'A1':'A1',
-                'A2':'A2',
-                'A3':'A3',
-                'A4':'A4',
-                'A5':'A5',
-                'A6':'A6',
-                'P1':'P1',
-                'P2':'P2',
-                'P3':'P3',
-                'P4':'P4',
-                'P5':'P5',
-                'P6':'P6'
+                'd1':'(2*pi/Ured)*P1',    'd2':'(2*pi/Ured)**2*P4', 'd3':'(2*pi/Ured)*P5',
+                'd4':'(2*pi/Ured)**2*P6', 'd5':'(2*pi/Ured)*P2',    'd6':'(2*pi/Ured)**2*P3',
+                'l1':'(2*pi/Ured)*H5',    'l2':'(2*pi/Ured)**2*H6', 'l3':'(2*pi/Ured)*H1',
+                'l4':'(2*pi/Ured)**2*H4', 'l5':'(2*pi/Ured)*H2',    'l6':'(2*pi/Ured)**2*H3',
+                'm1':'(2*pi/Ured)*A5',    'm2':'(2*pi/Ured)**2*A6', 'm3':'(2*pi/Ured)*A1',
+                'm4':'(2*pi/Ured)**2*A4', 'm5':'(2*pi/Ured)*A2',    'm6':'(2*pi/Ured)**2*A3'
             }
-        }
-    },
-    {
-        'name':'classic',
-        'relations':{
-            'H1':'H1/2',
-            'H2':'H2/2',
-            'H3':'H3/2',
-            'H4':'H4/2',
-            'H5':'H5/2',
-            'H6':'H6/2',
-            'A1':'A1/2',
-            'A2':'A2/2',
-            'A3':'A3/2',
-            'A4':'A4/2',
-            'A5':'A5/2',
-            'A6':'A6/2',
-            'P1':'P1/2',
-            'P2':'P2/2',
-            'P3':'P3/2',
-            'P4':'P4/2',
-            'P5':'P5/2',
-            'P6':'P6/2'
         }
     }
 ]
