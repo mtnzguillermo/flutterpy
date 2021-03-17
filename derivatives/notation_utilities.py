51af8e7ea31d84253af258ff02d5ac469d4438ee
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-03-17 17:31:07 +0100

    Notation input configuration

--- derivatives/notation_utilities.py
+++ derivatives/notation_utilities.py
@@ -5,27 +5,3 @@ class Notation():
 
     def __init__(self):
         pass
-
-
-class RealNotation(Notation):
-
-    def __init__(self):
-        
-        super().__init__()
-
-
-class ComplexNotation(Notation):
-
-    def __init__(self):
-
-        self.name = 
-
-        relations = {
-            'c_hh':{
-                'H4': 2/np.pi,
-                'H1': 2/np.pi*j
-            }
-        }
-
-        super().__init__()
-
