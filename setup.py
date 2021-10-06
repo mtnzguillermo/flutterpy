fb25c349d5fb392a69a3e8b3fb29073bc8f62640
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-10-06 13:42:31 +0200

    Metadata configuration

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="flutterpy",
-    version="0.1.0",
-    description="Package for the evaluation of wind-induced flutter effects in structures.",
-    packages=find_packages(exclude=("tests","docs"))
+    name = "flutterpy",
+    author = "Guillermo Martínez-López",
+    author_email = "guillermo.martinez-lopez@tum.de",
+    version = "0.0.0",
+    description = "Package for the evaluation of wind-induced flutter effects in structures.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/mtnzguillermo/flutterpy",
+    python_requires = ">=3.7",
+    install_requires = [
+        "numpy >= 1.17.4",
+        "scipy >= 1.6.3",
+        "sympy >= 1.5.1",
+        "matplotlib >= 3.2.0"
+    ],
+    extras_requires = {
+        "dev":[
+            
+        ]
+    },
+    keywords = "flutter, wind, aeroelasticity",
+    packages = find_packages(exclude=("tests","docs")),
+    classifiers = [
+
+    ]
 )
