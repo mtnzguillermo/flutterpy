70b3ea23e6d7e693992916800d443249d91d2e42
Guillermo Martínez-López <mtnzguillermo@gmail.com>
2021-10-11 07:56:18 +0200

    Move functions to utilities

--- flutterpy/derivatives/notations/utilities.py
+++ flutterpy/derivatives/notations/utilities.py
@@ -1,53 +1,32 @@
 
-import numpy as np
+from flutterpy.derivatives.notations.notation import _check_notation_key, available_notations
 
-
-def complex2real_notation(fd_complex):
-
-    fd_real = {'H1':{'values':[],'U_red':[]},
-        'H2':{'values':[],'U_red':[]},
-        'H3':{'values':[],'U_red':[]},
-        'H4':{'values':[],'U_red':[]},
-        'A1':{'values':[],'U_red':[]},
-        'A2':{'values':[],'U_red':[]},
-        'A3':{'values':[],'U_red':[]},
-        'A4':{'values':[],'U_red':[]}}
+def notation2notation(fd_in, notation_in, notation_out):
     
-    for letter_1 in 'ha':
+    fd_base = notation2base(fd_in, notation_in)
+    fd_out = base2notation(fd_base, notation_out)
 
-        if letter_1 == 'a':
-            factor_1 = 0.5
-            deriv_letter = 'A'
-        else:
-            factor_1 = 1
-            deriv_letter = 'H'
+    return fd_out
 
-        for letter_2 in 'ha':
-            
-            if letter_2 == 'a':
-                factor_2 = 0.5
-                deriv_numbers = [2,3]
-            else:
-                factor_2 = 1
-                deriv_numbers = [1,4]
 
-            for input_value, k in zip(fd_complex['c_'+letter_1+letter_2]['values'], fd_complex['c_'+letter_1+letter_2]['k']):
+def notation2base(fd_in, notation_in):
 
-                U_red = np.pi / k
-
-                output_values = np.array([np.imag(input_value), np.real(input_value)]) * np.pi * 0.5 * factor_1 * factor_2
-
-                for output_value, deriv_number in zip(output_values, deriv_numbers):
-                    fd_real[deriv_letter+str(deriv_number)]['values'].append(output_value)
-                    fd_real[deriv_letter+str(deriv_number)]['U_red'].append(U_red)
+    _check_notation_key(notation_in)
     
-    return fd_real
-                
-
-def real2complex_notation(fd_real):
-    pass
+    fd_out = available_notations[notation_in].deconvert(fd_in)
+    
+    return fd_out
 
 
+def base2notation(fd_in, notation_out):
+
+    _check_notation_key(notation_out)
+    
+    fd_out = available_notations[notation_out].convert(fd_in)
+    
+    return fd_out
+
+'''
 if __name__ == '__main__':
     fd_complex = {'c_aa':{'values':[2.4-0.36j],'k':[0.5]},
         'c_ah':{'values':[0.0071+1.2j],'k':[0.5]},
@@ -56,3 +35,4 @@ if __name__ == '__main__':
 
     fd_real = complex2real_notation(fd_complex)
     print(fd_real)
+'''
